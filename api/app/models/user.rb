3fa5291ffbe9082a69cf52fd5b3213bfbbb1232c
nick <mwauranicholas10@gmail.com>
2023-04-21 18:31:48 +0300

    api:added cart info in user.rb

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :orders
-    
+    has_one :cart, dependent: :destroy
+
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :presence, length: { minimum: 6 }
