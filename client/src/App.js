bfe770a23e504012ecbadc50df037aa41733365e
Ayub Ian <iayub.mia@gmail.com>
2023-04-21 17:21:41 +0300

    client: add navbar to products page

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,5 @@
-/*import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css';
-import Products from './components/pages/products/Product';
-import Services from './components/pages/services/Services';
-import LandingPage from './components/pages/landing-page/LandingPage'
-import LoginPage from './components/pages/auth/LoginPage'
-import RegisterPage from './components/pages/auth/RegisterPage'
-import ForgetPasswordPage from './components/pages/auth/ForgetPasswordPage'
-import HomePage from './components/pages/homepage/HomePage'
-
-
-
-
-function App() {
-  
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-       
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/forget-password" element={<ForgetPasswordPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path='services' element={<Services />} />
-            <Route path='products' element={<Products />} />
-      </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;*/
-
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Products from "./components/products/Products";
-import ProductDetail from './components/products/ProductDetails'
-import Cart from './components/cart/Cart'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Services from './components/pages/services/Services';
 import LandingPage from './components/pages/landing-page/LandingPage'
@@ -45,6 +7,9 @@ import LoginPage from './components/pages/auth/LoginPage'
 import RegisterPage from './components/pages/auth/RegisterPage'
 import ForgetPasswordPage from './components/pages/auth/ForgetPasswordPage'
 import HomePage from './components/pages/homepage/HomePage'
+import ProductDetail from "./components/pages/products/ProductDetails";
+import Products from "./components/pages/products/Products";
+import Cart from "./components/pages/cart/Cart";
 
 
 function App() {
